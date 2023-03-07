0bc42f16acac96bd5129bc4e0d824c5d30c8760a
kdany25 <kabadany25@gmail.com>
2023-03-07 15:41:45 +0200

    feat:added a header

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
 	return (
 		<Provider store={store}>
-			<View style={styles.container}>
-				<Text>Open up App.tsx to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
+			<SafeAreaProvider>
+				<HomeScreen />
+			</SafeAreaProvider>
 		</Provider>
 	);
 }
