06df830176933fd1218e7b93cd33499058ac9dbe
kdany25 <kabadany25@gmail.com>
2023-03-14 12:02:13 +0200

    feat: added navigation favourite

--- components/NavigateCard.tsx
+++ components/NavigateCard.tsx
@@ -6,6 +6,9 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navslice";
 import { useNavigation } from "@react-navigation/native";
+import NavFavourites from "./NavFavourites";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
 
 const NavigateCard = () => {
 	const dispatch = useDispatch();
@@ -38,6 +41,36 @@ const NavigateCard = () => {
 						}}
 					/>
 				</View>
+				<NavFavourites />
+			</View>
+
+			<View
+				style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}
+			>
+				<TouchableOpacity
+					style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+					onPress={() => navigation.navigate("RideOptionsCard")}
+				>
+					<Icon
+						name="car"
+						type="font-awesome"
+						color="white"
+						size={16}
+					/>
+					<Text style={tw`text-white text-center`}>Rides</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+				>
+					<Icon
+						name="fast-food-outline"
+						type="ionicon"
+						color="black"
+						size={16}
+					/>
+					<Text style={tw`text-center`}>Eats</Text>
+				</TouchableOpacity>
 			</View>
 		</SafeAreaView>
 	);
